796acf503e3fcaf663f143ffe2be47e1ec8b74bb
Saschakl <saschadavidknobel@gmail.com>
2021-11-06 18:18:23 +0100

    Code angepasst

--- data-preparation/data.py
+++ data-preparation/data.py
@@ -1,3 +1,4 @@
+from re import X
 import pandas as pd
 import numpy as np
 from IPython.display import display
@@ -6,24 +7,47 @@ import io
 
 
 
-df = pd.read_csv("verkehrszahlung.csv", sep=";", error_bad_lines=False).fillna(0)
+#df = pd.read_csv("verkehrszahlung.csv", sep=";", error_bad_lines=False).fillna(0)
 
 dfg = pd.read_csv("verkehrszahlung_gesamt_2018_2020.csv", sep=";", error_bad_lines=False).fillna(0) 
 
-dfg = dfg.drop (dfg.columns[[4,5,6,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]], axis=1)
+dfg = dfg.drop (dfg.columns[[0,4,5,6,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,32]], axis=1)
 dfg.insert(4, "JAHR", value=None)
-dfg['JAHR'] = dfg['DATUM'].str.slice(0, 3)
+dfg.insert(2, "Longitude", value=None)
+dfg['JAHR'] = dfg['DATUM'].str.slice(0,4)
+dfg = dfg.drop (dfg.columns[[3]], axis=1)
+dfg[['Longitude','Laltitude']] = dfg['STANDORT'].str.split(',',1, expand=True)
+dfg = dfg.drop (dfg.columns[1], axis=1)
+dfg = dfg[['BEZEICHNUNG', 'Longitude', 'Laltitude', 'RICHTUNG', 'JAHR', 'TAGESTOTAL']]
 
-df = df.drop (df.columns[[3,4,5,6,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37]], axis=1)
-
+#display[(dfg['JAHR'] == 2020) & (dfg['BEZEICHNUNG'] == 'St.Gallen Stadt Gaiserwaldstr.')]
 
 def Total(year, region):
-    dfg.loc[(df['JAHR'] == year) & (df['BEZEICHNUNG'] == region), 'TAGESTOTAL'].sum()
+    s = dfg[(dfg['JAHR'] == year) & (dfg['BEZEICHNUNG'] == region)]
+    sum = s['TAGESTOTAL'].sum()
+    display (sum)
 
 
-df.loc[df['ORT-ID']==10944][df['NAME_D']=="Bus, Car"]
+#Total(2018, "St.Gallen Stadt Gaiserwaldstr.")
+
+
+#def ganz(Jahr, Bezeichnung):
+ #   J = dfg.loc[dfg['JAHR']==Jahr][dfg['BEZEICHNUNG']=="Lastwagen"]
+  #  s = J['TAGESTOTAL'].sum()
+   # display(s)
+
+
+#ganz(2020, "St.Gallen Stadt Gaiserwaldstr.")
+#df = df.drop (df.columns[[3,4,5,6,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37]], axis=1)
+
+
+#def Total(year, region):
+#    dfg.loc[(df['JAHR'] == year) & (df['BEZEICHNUNG'] == region), 'TAGESTOTAL'].sum()
+
+
+#df.loc[df['ORT-ID']==10944][df['NAME_D']=="Bus, Car"]
 #TotJosefenbc = Josefenbc['TAGESTOTAL'].sum()
-Josefenlz = df.loc[(df['ORT-ID'] == 10944) & (df['NAME_D'] == "Lastenzug"), 'TAGESTOTAL'].sum()
+#Josefenlz = df.loc[(df['ORT-ID'] == 10944) & (df['NAME_D'] == "Lastenzug"), 'TAGESTOTAL'].sum()
 #TotJosefenlz = Josefenlz['TAGESTOTAL'].sum()
 #Josefenlkw = df.loc[df['ORT-ID']==10944][df['NAME_D']=="Lastwagen"]
 #TotJosefenlkw = Josefenlkw['TAGESTOTAL'].sum()
@@ -83,7 +107,3 @@ Josefenlz = df.loc[(df['ORT-ID'] == 10944) & (df['NAME_D'] == "Lastenzug"), 'TAG
 
 
 #Josefens = df.loc[df['ORT-ID']==10944], ['NAME_D']=="Bus, Car"
-
-display(Josefenlz)
-
-display(dfg)
