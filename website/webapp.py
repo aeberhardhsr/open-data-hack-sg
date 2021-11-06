380288d35a1393761834fd6118cb1c2653338ecd
aeberharhsr <19borki94@gmail.com>
2021-11-06 15:04:46 +0100

    add container for geomap

--- website/webapp.py
+++ website/webapp.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import numpy as np
+from geomap import geomap_yearly_overview
+import pandas as pd
 
 # data import and calculations
+latitude_list = [47.4268991755, 47.4151264537, 47.4133813496, 47.4226367328, 47.4048725228 ]
+longitude_list = [ 9.383057054, 9.34173656537, 9.34717463867, 9.34185926089, 9.30853184166 ]
+circle_size = 50
+circle_color = "#4fffff"
+
+#htmlmap = geomap_yearly_overview(latitude_list, longitude_list, circle_size, circle_color)
 
 
 ## Sidebar
@@ -36,6 +44,16 @@ count_station = st.sidebar.selectbox("Standort auswählen", options=[
 st.title("Verkehrszählung St. Gallen")
 st.write("Auswertung des gesamten Verkehrsflusses an den jeweiligen Aufzeichnungsorten über drei Jahre hinweg")
 st.caption("Jahr {}".format(year))
+#st.markdown(htmlmap, unsafe_allow_html=True)
+data = pd.DataFrame({
+    'counter_locations' : ["Bildweiherstrasse", "Fürstenlandstrasse 57", "Lerchenfeld", "Singenberg", "St. Josefen-Strasse"],
+    'lat' : [47.4268991755, 47.4151264537, 47.4133813496, 47.4226367328, 47.4048725228],
+    'lon' : [9.383057054, 9.34173656537, 9.34717463867, 9.34185926089, 9.30853184166]
+})
+
+
+st.map(data)
+
 
 st.title("Verkehrszählung nach Fahrzeugkategorie")
 st.write("Auswertung des gesamten Verkehrsflusses kategorisiert nach Fahrzeugtyp")
