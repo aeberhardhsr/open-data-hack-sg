dc090174e73c1e0235e700916f0ead2e2355d4ef
aeberharhsr <19borki94@gmail.com>
2021-11-07 00:39:41 +0100

    add data from backend to frontend with functional user input

--- website/webapp.py
+++ website/webapp.py
@@ -2,13 +2,28 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+ 
+
+
 
 
 ## Sidebar
 st.sidebar.markdown("## Einstellungen")
 st.sidebar.markdown("Jahr auswählen um die spezifische Verkehrsdichte anzuzeigen")
 year = st.sidebar.slider('Aufzeichnungs Jahr', min_value=2018, max_value=2020, step=1)
+st.sidebar.markdown("Zählerstation auswählen")
+count_station_year = st.sidebar.selectbox("Standort auswählen", options=[
+    "Bildweiherstrasse",
+    "Fürstenlandstrasse 57",
+    "Lerchenfeld",
+    "Singenberg",
+    "St. Josefen-Strasse",
+    "Teufener Strasse 75"
+], key=1)
 st.sidebar.markdown("___")
+
+
+year_radio = st.sidebar.radio("Jahr auswählen", ("2019", "2020"))
 st.sidebar.markdown("Fahrzeugkategorie")
 veh_category = st.sidebar.selectbox("Kategorie auswählen", options=[
     "Bus, Car",
@@ -24,38 +39,87 @@ veh_category = st.sidebar.selectbox("Kategorie auswählen", options=[
     ])
 st.sidebar.markdown("Zählerstation auswählen")
 count_station = st.sidebar.selectbox("Standort auswählen", options=[
-    "Bildweiherstrasse",
-    "Fürstenlandstrasse 57",
-    "Lerchenfeld",
-    "Singenberg",
-    "St. Josefen-Strasse",
-    "Teufener Strasse 75"
+    "St.Gallen Stadt Bildweiherstr.",
+    "St.Gallen Stadt Lerchenfeld",          
+    "St.Gallen Stadt Singenberg",           
+    "St.Gallen Stadt St.Josefen-Str",       
+    "St.Gallen Stadt Teufener Str.75"
 ])
+
+
+### begin data preparation for first map ###
+
+### end data preparation for first map ###
+
+
+### begin data preparation for second map ###
+
+df = pd.read_csv("verkehrszahlung.csv", sep=";", error_bad_lines=False)
+# drop non usable columns
+df = df.drop([
+    'WOCHENTAG',
+    'ARBEITSTAG',
+    'KENNZAHL',
+    'RICHTUNG',
+    'ORT-RICHTUNG ID',
+    'KLASSE',
+    'SWISS10GROUP',
+    'SWISS7GROUP',
+    'SWISS6GROUP',
+    '1', '2', '3', '4',
+    '5', '6', '7', '8',
+    '9', '10', '11', '12',
+    '13', '14', '15', '16',
+    '17', '18', '19', '20',
+    '21', '22', '23', '24'], 1)
+
+# replace chars in columns
+df['STANDORT'] = df['STANDORT'].str.replace('"', '')
+df['NAME_D'] = df['NAME_D'].str.replace('"', '')
+
+# split the standort columnt to extract coordinates
+df[['Latitude','Longitude']] = df['STANDORT'].str.split(',',expand=True)
+
+# extract year for slider on website
+df['JAHR'] = pd.DatetimeIndex(df['DATUM']).year
+
+# drop non used columns
+df = df.drop(['STANDORT', 'DATUM'], 1)
+
+result = df.loc[(df['BEZEICHNUNG'] == count_station) & (df['JAHR'] == int(year_radio)) & (df['NAME_D'] == veh_category)]['TAGESTOTAL'].sum()
+
+### end of data preparation ###
 
 # Main 
 st.title("Verkehrszählung St. Gallen")
 st.write("Auswertung des gesamten Verkehrsflusses an den jeweiligen Aufzeichnungsorten über drei Jahre hinweg")
-st.caption("Jahr {}".format(year))
-#st.markdown(htmlmap, unsafe_allow_html=True)
+
+# first map
 data = pd.DataFrame({
     'counter_locations' : ["Bildweiherstrasse", "Fürstenlandstrasse 57", "Lerchenfeld", "Singenberg", "St. Josefen-Strasse"],
     'lat' : [47.4268991755, 47.4151264537, 47.4133813496, 47.4226367328, 47.4048725228],
-    'lon' : [9.383057054, 9.34173656537, 9.34717463867, 9.34185926089, 9.30853184166]
-
-    dfg = pd.read_csv("verkehrszahlung_gesamt_2018_2020.csv", sep=";", error_bad_lines=False).fillna(0) 
-    dfg = dfg.drop (dfg.columns[[4,5,6,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]], axis=1)
-    dfg.insert(4, "JAHR", value=None)
-    dfg['JAHR'] = dfg['DATUM'].str.slice(0, 3)
-
-df = df.drop (df.columns[[3,4,5,6,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37]], axis=1)
-})
-
-
+    'lon' : [9.383057054, 9.34173656537, 9.34717463867, 9.34185926089, 9.30853184166]})
 st.map(data)
 
+col1, col2 = st.columns(2)
+col1.metric("Jahr", year)
+col2.metric("Fahrzeugzähler", "11Mio", "5%")
+st.metric("Standort", count_station_year)
+st.markdown("___")
 
 st.title("Verkehrszählung nach Fahrzeugkategorie")
 st.write("Auswertung des gesamten Verkehrsflusses kategorisiert nach Fahrzeugtyp")
-st.caption("Fahrzeugkategorie: {} | Zählungsstandort: {}".format(veh_category, count_station))
 
+# second map
+data2 = pd.DataFrame({
+    'counter_locations' : ["Bildweiherstrasse", "Fürstenlandstrasse 57", "Lerchenfeld", "Singenberg", "St. Josefen-Strasse"],
+    'lat' : [47.4268991755, 47.4151264537, 47.4133813496, 47.4226367328, 47.4048725228],
+    'lon' : [9.383057054, 9.34173656537, 9.34717463867, 9.34185926089, 9.30853184166]})
 
+st.map(data2)
+
+col1, col2 = st.columns(2)
+col1.metric("Fahrzeugzähler", int(result), "5%")
+col2.metric("Jahr", year_radio)
+st.metric("Kategorie", veh_category)
+st.metric("Standort", count_station)
