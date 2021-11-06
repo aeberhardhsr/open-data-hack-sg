77b5b96e77d22a8b550e716dc344945e1ab52ce4
ADG@DESKTOP-QJN6N2P MINGW64 ~$ ls 01_Projekte/ 02_CpS-Doctor/'3D Objects'/'Adaptive Gaussian.png''Adaptive Mean.png' Anwendungsdaten@ AppData/ Backup/ Binary.png Calibre-Bibliothek/ Code/ Contacts/ Cookies@ Desktop/ Documents/ Downloads/ Druckumgebung@'Eigene Dateien'@ Favorites/ Gray.png Hardware/ IntelGraphicsProfiles/ Links/'Lokale Einstellungen'@ MicrosoftEdgeBackups/ Music/ NTUSER.DAT NTUSER.DAT{8d736fb1-9fd3-11eb-b03c-9f4867261a69}.TM.blf NTUSER.DAT{8d736fb1-9fd3-11eb-b03c-9f4867261a69}.TMContainer00000000000000000001.regtrans-ms NTUSER.DAT{8d736fb1-9fd3-11eb-b03c-9f4867261a69}.TMContainer00000000000000000002.regtrans-ms Netzwerkumgebung@ OneDrive/ Original.png Pictures/ Recent@ Rotated.png'Saved Games'/ Scaled.png Searches/ SendTo@ Spitfire/ Startmenü@ Translated.png Videos/ Vorlagen@ darknet/ miniconda3/ ntuser.dat.LOG1 ntuser.dat.LOG2 ntuser.ini tutorial_env/ADG@DESKTOP-QJN6N2P MINGW64 ~$ cd Code/open-data-hack-sg/ADG@DESKTOP-QJN6N2P MINGW64 ~/Code/open-data-hack-sg (main)$ git config user.email aaron.gitz@gmail.comADG@DESKTOP-QJN6N2P MINGW64 ~/Code/open-data-hack-sg (main)$ git config --global user.name <aaron.gitz@gmail.com>
2021-11-07 00:31:48 +0100

    almost done ;)

--- data-preparation/geocode.py
+++ data-preparation/geocode.py
@@ -3,7 +3,7 @@ import requests
 def geo_code_swisstopo(addressString=None):
     params = { 'type': 'locations',
                 'searchText': addressString,
-                'sr': 2056,
+                'sr': 4376,
                 'limit': 1 }
 
     res = requests.get("https://api3.geo.admin.ch/rest/services/ech/SearchServer?", params=params)
@@ -11,10 +11,8 @@ def geo_code_swisstopo(addressString=None):
         result = res.json()
         if "results" in result.keys() and len(result["results"]) > 0:
             likely_match = result["results"][0]["attrs"]
-            return {
-                    'latitude_lv95': int(likely_match['x']), 
-                    'longitude_lv95' : int(likely_match['y']),
-                    'egid' : likely_match["featureId"]}
+            with open ('coordinates.py', 'w') as f:
+                f.write("%s\n" % 'latitude_lv95: {}'.format(int(likely_match['x'])), 'longitude_lv95: {}'.format(int(likely_match['y'])))
         else:
-            print(f'swisstopo no result while geocoding for '{addressString}'')
+            print(f'swisstopo no result while geocoding for {addressString}')
             return {}
