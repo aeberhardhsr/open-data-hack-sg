6704368c4e794b9a5a948df3bfe53829fd9c0545
aeberharhsr <19borki94@gmail.com>
2021-11-06 01:18:51 +0100

    script to localize traffic counter

---
+++ geomap.py
@@ -0,0 +1,21 @@
+# import gmplot package
+import gmplot
+
+# API KEY = AIzaSyAVsapOCYYsZ3pl8sCC0i04g6UuH7H3fUQ
+apikey = 'AIzaSyAVsapOCYYsZ3pl8sCC0i04g6UuH7H3fUQ'
+
+
+latitude_list = [47.4268991755, 47.4151264537, 47.4133813496, 47.4226367328, 47.4048725228 ]
+longitude_list = [ 9.383057054, 9.34173656537, 9.34717463867, 9.34185926089, 9.30853184166 ]
+  
+gmap3 = gmplot.GoogleMapPlotter(47.4, 9.3, 13, apikey=apikey)
+  
+# scatter method of map object 
+# scatter points on the google map
+gmap3.scatter( latitude_list, longitude_list, '#FF0000', size = 40, marker = False )
+  
+# Plot method Draw a line in
+# between given coordinates
+#gmap3.plot(latitude_list, longitude_list, 'cornflowerblue', edge_width = 2.5)
+  
+gmap3.draw("map.html")
