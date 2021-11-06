d947f079f03beb742c2a65736a05a1ab97fd01a0
ADG@DESKTOP-QJN6N2P MINGW64 ~$ ls 01_Projekte/ 02_CpS-Doctor/'3D Objects'/'Adaptive Gaussian.png''Adaptive Mean.png' Anwendungsdaten@ AppData/ Backup/ Binary.png Calibre-Bibliothek/ Code/ Contacts/ Cookies@ Desktop/ Documents/ Downloads/ Druckumgebung@'Eigene Dateien'@ Favorites/ Gray.png Hardware/ IntelGraphicsProfiles/ Links/'Lokale Einstellungen'@ MicrosoftEdgeBackups/ Music/ NTUSER.DAT NTUSER.DAT{8d736fb1-9fd3-11eb-b03c-9f4867261a69}.TM.blf NTUSER.DAT{8d736fb1-9fd3-11eb-b03c-9f4867261a69}.TMContainer00000000000000000001.regtrans-ms NTUSER.DAT{8d736fb1-9fd3-11eb-b03c-9f4867261a69}.TMContainer00000000000000000002.regtrans-ms Netzwerkumgebung@ OneDrive/ Original.png Pictures/ Recent@ Rotated.png'Saved Games'/ Scaled.png Searches/ SendTo@ Spitfire/ Startmenü@ Translated.png Videos/ Vorlagen@ darknet/ miniconda3/ ntuser.dat.LOG1 ntuser.dat.LOG2 ntuser.ini tutorial_env/ADG@DESKTOP-QJN6N2P MINGW64 ~$ cd Code/open-data-hack-sg/ADG@DESKTOP-QJN6N2P MINGW64 ~/Code/open-data-hack-sg (main)$ git config user.email aaron.gitz@gmail.comADG@DESKTOP-QJN6N2P MINGW64 ~/Code/open-data-hack-sg (main)$ git config --global user.name <aaron.gitz@gmail.com>
2021-11-06 13:54:46 +0100

    with User-Agent

--- webscraping/scraper.py
+++ webscraping/scraper.py
@@ -1,11 +1,10 @@
 import requests
-import lxml
 from bs4 import BeautifulSoup
 
 #response = requests.get('https://ch.kompass.com/searchCompanies?acClassif=&localizationCode=CH_17_1721_3203&localizationLabel=St.+Gallen&localizationType=town&text=&searchType=SUPPLIER')
-result = requests.get('https://ch.kompass.com/c/debrunner-koenig-ag/ch120311/')
+result = requests.get('https://ch.kompass.com/c/debrunner-koenig-ag/ch120311/', headers={ 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'})
 src = result.content
-soup = BeautifulSoup(src, 'lxml')
+soup = BeautifulSoup(src, 'html.parser')
 
-tag = soup.find_all('span', itemprop='streetAddress')
+tag = soup.find_all('span', class_='spRight')
 print(tag)
